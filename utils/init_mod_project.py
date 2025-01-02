67ac18e5ebd0aba12d7c77f1dc56e6e74475ab48
slafniy <slafniy@gmail.com>
2025-01-02 04:46:19 +0400

    init_mod_project.py - can create junctions

--- utils/init_mod_project.py
+++ utils/init_mod_project.py
@@ -1,7 +1,8 @@
 import argparse
+import os
 
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 DEFAULT_DATA_PATH = r'C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data'
 JUNCTION_TARGETS = [
@@ -17,11 +18,10 @@ class _Runner:
 
         self.data_folder: Path = self.ensure_data_folder()
         self.mod_name: str = self.ensure_mod_name()
-        self.mod_folders: List[Path] = self.ensure_mod_folders()
-        self.project_folder: Path = self.ensure_project_folder()
+        self.project_root: Path = self.ensure_project_root()
+        self.junction_map : Dict[Path, Path] = self.prepare_junction_map()  # source: target
 
-    def get_orig_mod_folders(self, data_path: Path):
-        pass
+        self.make_junctions()
 
 
     def ensure_data_folder(self) -> Path:
@@ -40,19 +40,8 @@ class _Runner:
         print(f'"{self.arguments.name}" mod unique name: {mod_name}')
         return mod_name
 
-    def ensure_mod_folders(self) -> List[Path]:
-        result = []
-        for p in JUNCTION_TARGETS:
-            mf = Path(self.data_folder / p / self.mod_name)
-            assert mf.exists(), f'"{mf}" does not exist!'
-            assert mf.is_dir(), f'"{mf}" is not a directory!'
-            result.append(mf)
 
-        print('Mod folders:')
-        [print(mf) for mf in result]
-        return result
-
-    def ensure_project_folder(self) -> Path:
+    def ensure_project_root(self) -> Path:
         path = Path(self.arguments.path) if self.arguments.path is not None else Path.cwd() / self.arguments.name
         assert path.is_dir() or not path.exists(), f'"{path}" is not a directory!'
 
@@ -62,8 +51,31 @@ class _Runner:
         print(f'Project folder: {path}')
         return path
 
+    def make_junctions(self):
+        for source, target in self.junction_map.items():
+            cmd = f'mklink /J "{target}" "{source}"'
+            print(f'Executing "{cmd}"')
+            os.system(cmd)
+
+    def prepare_junction_map(self) -> Dict[Path, Path]:
+        result = {}
+        for p in JUNCTION_TARGETS:
+            mod_source = Path(self.data_folder / p / self.mod_name)
+            assert mod_source.exists(), f'"{mod_source}" does not exist!'
+            assert mod_source.is_dir(), f'"{mod_source}" is not a directory!'
+
+            mod_target = self.project_root / p / self.mod_name
+            assert mod_target.is_dir() or not mod_target.exists(), f'"{mod_target}" is not a directory!'
+            os.makedirs(mod_target.parent, exist_ok=True)
+
+            result[mod_source] = mod_target
+
+        return result
+
 
 if __name__ == '__main__':
+    assert os.name == 'nt', 'This scripts creates directory junctions and works only on Windows'
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-n', '--name', help='Project name without GUID part', required=True)
     arg_parser.add_argument('-p', '--path', help='Path to the project directory. '
